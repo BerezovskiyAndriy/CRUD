90839c4de53d476d88ec85b9801279067bd60e33
AndriyBerezovskiy <andriyxox@gmail.com>
2022-03-09 02:01:57 +0200

    -

--- lesson5/src/app.ts
+++ lesson5/src/app.ts
@@ -1,12 +1,41 @@
 import 'reflect-metadata';
-import express from 'express';
-import { createConnection } from 'typeorm';
+import express, { Request, Response } from 'express';
+import { createConnection, getManager } from 'typeorm';
+import { User } from './entity/user';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', ((req, res) => {
-    res.end();
-}));
+app.get('/users', async (req: Request, res: Response) => {
+    const users = await getManager().getRepository(User).find({ relations: ['posts'] });
+    console.log(users);
+    res.json(users);
+});
+
+app.post('/users', async (req:Request, res:Response) => {
+    const createdUser = await getManager().getRepository(User).save(req.body);
+    res.json(createdUser);
+});
+
+app.patch('/users/:id', async (req:Request, res:Response) => {
+    const { password, email } = req.body;
+    const createdUser = await getManager().getRepository(User).update(
+        { id: Number(req.params.id) },
+        {
+            password,
+            email,
+        },
+    );
+    res.json(createdUser);
+});
+
+app.delete('/users/:id', async (req: Request, res:Response) => {
+    const createdUser = getManager().getRepository(User).delete(
+        { id: Number(req.params.id) },
+    );
+    res.json(createdUser);
+});
 
 app.listen(5400, async () => {
     console.log('Server has started!!!');
